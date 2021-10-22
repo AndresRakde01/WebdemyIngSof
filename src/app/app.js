e06faaf1463d48bad08995c1c49d586d246cb34e
maurihv13 <huaytamauricio1@gmail.com>
2021-10-22 11:47:27 -0400

    Agregando parte frontend a la conexion con BD

---
+++ src/app/app.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+    render() {
+        return (
+            <div>
+                <nav className="light-blue darken-4">
+                    <div className="container">
+                        <a className="brand-logo" href="/"> Prueba MERN </a>
+                    </div>
+                </nav>
+
+                <div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
