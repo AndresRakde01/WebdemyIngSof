8795fa327ff8bd446989718fc89a7a399774c192
fred <alfredoLazaro999@gmail.com>
2021-10-24 09:10:58 -0400

    cree nueva consulta para busqueda por etiqueta

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,11 @@ router.get('/cursoU',async (req,res)=>{
     const cursoUnico = await pool.query('SELECT * FROM CURSO where id_curso = ?',5113);
     res.send(cursoUnico);
 });
+
+router.get('/etiqueta', async (req,res)=>{
+    //const etiq=
+    const cursoEti = await pool.query('SELECT curso.nombre,curso.imagen,curso.inscritos,curso.descripcion,curso.requisitos,curso.duracion,curso.fechaCreacion FROM Etiqueta as E join CURSO Join curso_has_etiqueta  where id_curso = CURSO_id_curso and id_etiqueta=ETIQUETA_id_etiqueta and E.nombre= ?', 'python');
+    res.send(cursoEti);
+});
+
 module.exports = router;
